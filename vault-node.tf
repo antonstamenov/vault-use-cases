9557e5c55e965c7d8d9fabf92001a65cf96d0e11
Anton Stamenov <antons@helecloud.com>
2019-09-25 14:12:52 +0300

    create vault node

---
+++ vault-node.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["591542846629"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "vault-node" {
+  count         = 1
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
+  subnet_id     = module.vpc.private_subnets[0]
+  key_name      = "helecloud"
+
+  tags = {
+    "Name"          = "vault-node${count.index}"
+    "vault-cluster" = "vault-use-cases"
+  }
+}
+
+resource "aws_security_group" "vault-nodes" {
+  name = "vault-nodes"
+}
