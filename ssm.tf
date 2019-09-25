910aeef6b8c1cbbc81ac47331b399c743edd6800
Anton Stamenov <antons@helecloud.com>
2019-09-25 15:23:21 +0300

    attach ssm profile

---
+++ ssm.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_instance_profile" "ssm" {
+  name = "ssm-ssh-proxy"
+  role = aws_iam_role.ssm-ssh-proxy
+}
+
+resource "aws_iam_role" "ssm-ssh-proxy" {
+  name               = "ssm-ssh-proxy"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    sid     = "EC2AssumeRole"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-ssh-proxy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+  role       = aws_iam_role.ssm-ssh-proxy.id
+}
--- vault-node.tf
+++ vault-node.tf
@@ -1,33 +1,10 @@
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-  owners = [
-  "amazon"]
-
-  filter {
-    name = "name"
-    values = [
-    "amzn2-ami-hvm-2.0.*"]
-  }
-
-  filter {
-    name = "architecture"
-    values = [
-    "x86_64"]
-  }
-
-  filter {
-    name = "virtualization-type"
-    values = [
-    "hvm"]
-  }
-}
-
 resource "aws_instance" "vault-node" {
-  count         = 1
-  ami           = data.aws_ami.amazon-linux-2.id
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.private_subnets[0]
-  key_name      = "helecloud"
+  count                = 1
+  ami                  = data.aws_ami.amazon-linux-2.id
+  instance_type        = "t2.micro"
+  subnet_id            = module.vpc.private_subnets[0]
+  key_name             = "helecloud"
+  iam_instance_profile = aws_iam_instance_profile.ssm.id
 
   tags = {
     "Name"          = "vault-node${count.index}"
@@ -37,4 +14,24 @@ resource "aws_instance" "vault-node" {
 
 resource "aws_security_group" "vault-nodes" {
   name = "vault-nodes"
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
