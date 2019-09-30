49c403b92543b147cd50b941d8153f539880e015
Anton Stamenov <antons@helecloud.com>
2019-09-30 11:51:46 +0300

    add tag to be grouped in resource groups

--- vault-node.tf
+++ vault-node.tf
@@ -7,8 +7,9 @@ resource "aws_instance" "vault-node" {
   iam_instance_profile = aws_iam_instance_profile.vault-nodes.name
 
   tags = {
-    "Name"          = "vault-node${count.index}"
-    "vault-cluster" = "vault-use-cases"
+    "Name"         = "vault-node${count.index}"
+    "VaultCluster" = "vault-use-cases"
+    "Environment"  = "poc"
   }
 }
 
@@ -18,20 +19,24 @@ resource "aws_security_group" "vault-nodes" {
 
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
-  owners      = ["amazon"]
+  owners = [
+  "amazon"]
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.*"]
+    name = "name"
+    values = [
+    "amzn2-ami-hvm-2.0.*"]
   }
 
   filter {
-    name   = "architecture"
-    values = ["x86_64"]
+    name = "architecture"
+    values = [
+    "x86_64"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "virtualization-type"
+    values = [
+    "hvm"]
   }
 }
