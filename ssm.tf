9bf99766a7bc8b7f0d9e599d216a99a55c0c5c9a
Anton Stamenov <antons@helecloud.com>
2019-10-01 11:28:02 +0300

    add s3 backend

--- ssm.tf
+++ ssm.tf
@@ -23,3 +23,23 @@ resource "aws_iam_role_policy_attachment" "ssm-ssh-proxy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
   role       = aws_iam_role.ssm-ssh-proxy.name
 }
+
+
+data "aws_iam_policy_document" "vault-s3-backend" {
+  statement {
+    sid       = "VaultS3backend"
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["${aws_s3_bucket.vaultdb.arn}/*", "${aws_s3_bucket.vaultdb.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "vault-s3-backend" {
+  name   = "vault-s3-backend"
+  policy = data.aws_iam_policy_document.vault-s3-backend.json
+}
+
+resource "aws_iam_role_policy_attachment" "vault-s3-backend" {
+  policy_arn = aws_iam_policy.vault-s3-backend.arn
+  role       = aws_iam_role.ssm-ssh-proxy.name
+}
